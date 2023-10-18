e90ff32d39c49ac279ae7d1bf72f61e2719515c5
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-18 16:55:33 +0600

    AddProduct developed

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,9 +1,121 @@
-
+import Swal from 'sweetalert2';
 
 const AddProduct = () => {
+    const handleAddProduct = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const image = form.image.value;
+        const name = form.name.value;
+        const brandName = form.brandName.value;
+        const type = form.type.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const shortDescription = form.shortDescription.value;
+
+        const newProduct = { image, name, brandName, type, price, rating, shortDescription }
+
+        console.log(newProduct);
+
+        // Send data to the server
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Product Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    })
+                }
+            })
+    }
+
+
     return (
-        <div>
-            AddProduct
+        <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-3xl font-bold mb-7">Add a Product</h2>
+            <form onSubmit={handleAddProduct}>
+                {/* form image and name row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Image URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="image" placeholder="Image URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form brand name and type row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Brand Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="brandName" placeholder="Brand Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Type</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="type" placeholder="Type" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form price and rating row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="price" placeholder="Price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Rating</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="rating" placeholder="Rating" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form short description row */}
+                <div className="mb-8">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Short Description</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="shortDescription" placeholder="Short Description" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input type="submit" value="Add Product" className="btn btn-block text-white bg-[#FF444A] hover:bg-[#FF444A] normal-case" />
+
+            </form>
         </div>
     );
 };
