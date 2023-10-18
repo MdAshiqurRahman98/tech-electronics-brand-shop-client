aeb7038c8da2d534ce0f0cab7c60185b92484972
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-18 12:18:31 +0600

    ErrorPage updated

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
     return (
         <div className="text-center mt-52">
             <h3 className="text-3xl my-5">Oops!!!</h3>
-            <p className="mb-9">No page available</p>
+            <p className="mb-9 text-lg">The page doesn`t exist</p>
             <Link to='/'><button className="btn bg-[#FF444A] font-semibold text-white normal-case hover:bg-[#FF444A]">Go Home</button></Link>
         </div>
     );
