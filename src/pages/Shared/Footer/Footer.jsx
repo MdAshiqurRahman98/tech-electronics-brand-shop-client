3a02ddc1b9764693f1f6b197a58e1877cdd3d418
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-18 17:15:50 +0600

    footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,9 +2,29 @@
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer p-10 text-white">
+            <aside className="mt-10">
+                <p>Tech Brand Company Ltd.<br />Copyright &copy; Tech Brand | All rights reserved.</p>
+            </aside>
+            <nav>
+                <header className="footer-title">Brands</header>
+                <a className="link link-hover">Apple</a>
+                <a className="link link-hover">Samsung</a>
+                <a className="link link-hover">Sony</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Company</header>
+                <a className="link link-hover">Mission and vision</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Career</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Legal</header>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
     );
 };
 
