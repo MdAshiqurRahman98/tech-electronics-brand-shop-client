1c89bf153bfa7dac47ad3080b25eebffb915bb83
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-21 22:53:10 +0600

    banner updated

--- src/pages/Shared/Banner/Banner.jsx
+++ src/pages/Shared/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="lg:flex justify-evenly items-center mb-1">
             <h1 className="flex-1 text-5xl font-bold text-left ml-5">Technology and Electronics Brand Shop</h1>
-            <figure className="flex-1 -mr-36"><img className="w-[500px]" src="https://i.ibb.co/0tFR2j1/Logo.png" alt="Logo" /></figure>
+            <figure className="flex-1 lg:-mr-60"><img className="w-[500px]" src="https://i.ibb.co/0tFR2j1/Logo.png" alt="Logo" /></figure>
         </div>
     );
 };
