ce7a1eabf50ffaf1e57701f176b668ac0f7ac586
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-21 12:36:12 +0600

    products page updated

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import ProductsCard from "../ProductsCard/ProductsCard";
 import ProductsSlider from "../ProductsSlider/ProductsSlider";
 
@@ -7,17 +7,25 @@ const Products = () => {
 
     return (
         <div>
-            <div className="mb-11">
-                <ProductsSlider></ProductsSlider>
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-9'>
-                {
-                    loadedProducts.map(product => <ProductsCard
-                        key={product._id}
-                        product={product}
-                    ></ProductsCard>)
-                }
-            </div>
+            {
+                loadedProducts ? <>
+                    <div className="mb-11">
+                        <ProductsSlider></ProductsSlider>
+                    </div>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-9'>
+                        {
+                            loadedProducts.map(product => <ProductsCard
+                                key={product._id}
+                                product={product}
+                            ></ProductsCard>)
+                        }
+                    </div>
+                </> : <div className="text-center mt-52">
+                    <h3 className="text-3xl my-5">Oops!!!</h3>
+                    <p className="mb-9 text-lg">No product available</p>
+                    <Link to='/'><button className="btn bg-[#FF444A] font-semibold text-white normal-case hover:bg-[#FF444A]">Go Home</button></Link>
+                </div>
+            }
         </div>
     );
 };
--- src/pages/ProductsCard/ProductsCard.jsx
+++ src/pages/ProductsCard/ProductsCard.jsx
@@ -5,33 +5,24 @@ const ProductsCard = ({ product }) => {
     const { _id, image, name, brandName, type, price, rating } = product || {};
 
     return (
-        <>
-            {
-                product ? <div className="card card-side rounded-lg p-5 bg-[#F8F8F8] h-[235px]">
-                    <div>
-                        <img className="w-52 h-full" src={image} alt="Product" />
-                    </div>
-                    <div className="pl-7">
-                        <h3 className="text-lg font-bold mb-2">{name}</h3>
-                        <p className="font-medium mb-1">Brand: {brandName}</p>
-                        <p className="font-medium mb-1">Type: {type}</p>
-                        <p className="font-medium mb-1">Price: ${price}</p>
-                        <p className="font-medium mb-1">Rating: {rating}</p>
-                        <Link to={`/product/${_id}`}>
-                            <button className="btn btn-sm text-white bg-[#FF444A] hover:bg-[#FF444A] normal-case text-[15px] font-semibold rounded mt-3">Details</button>
-                        </Link>
-                        <Link to={`/updateProduct/${_id}`}>
-                            <button className="btn btn-sm text-white bg-[#FF444A] hover:bg-[#FF444A] normal-case text-[15px] font-semibold rounded mt-3 ml-3">Update</button>
-                        </Link>
-                    </div>
-                </div>
-                    : <div className="text-center mt-52">
-                        <h3 className="text-3xl my-5">Oops!!!</h3>
-                        <p className="mb-9 text-lg">No product available</p>
-                        <Link to='/'><button className="btn bg-[#FF444A] font-semibold text-white normal-case hover:bg-[#FF444A]">Go Home</button></Link>
-                    </div>
-            }
-        </>
+        <div className="card card-side rounded-lg p-5 bg-[#F8F8F8] h-[235px]">
+            <div>
+                <img className="w-52 h-full" src={image} alt="Product" />
+            </div>
+            <div className="pl-7">
+                <h3 className="text-lg font-bold mb-2">{name}</h3>
+                <p className="font-medium mb-1">Brand: {brandName}</p>
+                <p className="font-medium mb-1">Type: {type}</p>
+                <p className="font-medium mb-1">Price: ${price}</p>
+                <p className="font-medium mb-1">Rating: {rating}</p>
+                <Link to={`/product/${_id}`}>
+                    <button className="btn btn-sm text-white bg-[#FF444A] hover:bg-[#FF444A] normal-case text-[15px] font-semibold rounded mt-3">Details</button>
+                </Link>
+                <Link to={`/updateProduct/${_id}`}>
+                    <button className="btn btn-sm text-white bg-[#FF444A] hover:bg-[#FF444A] normal-case text-[15px] font-semibold rounded mt-3 ml-3">Update</button>
+                </Link>
+            </div>
+        </div>
     );
 };
 
